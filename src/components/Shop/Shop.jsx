7826a1a9963ad42c61a5d3c368bcbc5561a1fa07
swapnilRoyWeb <swapnilroyweb@gmail.com>
2023-03-30 13:50:28 +0600

    cart set to localstorage

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
@@ -13,12 +14,35 @@ const Shop = () => {
         fetch('products.json')
         .then(res => res.json())
         .then(data => setProducts(data))
-    },[])
+    },[]);
+
+    useEffect(() => {
+        const storedCart = getShoppingCart();
+        const savedCart = [];
+        // console.log(storedCart);
+        //step 1
+        for(const id in storedCart){
+            // console.log(id);
+            //step 2
+            const savedProduct = products.find(product => product.id === id);
+            // console.log(savedProduct);
+            if(savedProduct){
+                //step 3
+                const quantity = storedCart[id];
+                savedProduct.quantity = quantity;
+                //step 4
+                savedCart.push(savedProduct);
+            }
+        }
+        //step 5
+        setCart(savedCart);
+    }, [products]);
 
     const handleAddToCart = (product) => {
         // console.log('product added', product);
         const newCart = [...cart, product];
         setCart(newCart);
+        addToDb(product.id);
     }
 
     return (
