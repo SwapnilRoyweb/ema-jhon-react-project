3dbaa1beb62f2f1354f1250f80cc2e4f96dd5b5d
swapnilRoyWeb <swapnilroyweb@gmail.com>
2023-04-07 18:37:44 +0600

    order page ready and data loaded with functionality

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useLoaderData, useSearchParams } from 'react-router-dom';
+import { removeFromDb } from '../../utilities/fakedb';
+import Cart from '../Cart/Cart';
+import ReviewItem from '../ReviewItem/ReviewItem';
+import './Orders.css';
 
 const Orders = () => {
+    const savedCart = useLoaderData();
+    const [cart, setCart] = useState(savedCart);
+
+    const handleRemoveCart = (id) => {
+        // console.log(id);
+        const remaining = cart.filter(product => product.id !== id);
+        setCart(remaining);
+        removeFromDb(id);
+    }
+    
     return (
-        <div>
-            <h1>Orders Page</h1>
+        <div className='shop-container'>
+            <div className='review-container'>
+                {
+                    cart.map(product => <ReviewItem product= {product} key={product.id}handleRemoveCart={handleRemoveCart}></ReviewItem>)
+                }
+            </div>
+            <div>
+                <Cart cart={cart}></Cart>
+            </div>
         </div>
     );
 };
---
+++ src/loaders/CartProductsLoader.js
@@ -0,0 +1,26 @@
+import { getShoppingCart } from "../utilities/fakedb";
+
+const cartProductsLoader = async () => {
+    const loadedProducts = await fetch('products.json');
+    const products = await loadedProducts.json();
+
+    //if cart data is in database, you must have to async await
+    const storedCart = getShoppingCart();
+    const savedCart = [];
+
+    for(const id in storedCart){
+        const addedProduct = products.find(pd => pd.id === id);
+        if(addedProduct){
+            const quantity = storedCart[id];
+            addedProduct.quantity = quantity;
+            savedCart.push(addedProduct);
+        }
+    }
+    //if you need to send two things
+    //return [products, savedCart]
+
+    // console.log(products);
+    return savedCart;
+}
+
+export default cartProductsLoader;
