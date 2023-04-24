65c7d637f5913dee8fe36314b7a6b13df39be789
swapnilRoyWeb <swapnilroyweb@gmail.com>
2023-04-24 23:01:05 +0600

    firebase set up done and login and sign up page ready

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDTdMv14_FsnHeBwESwtLtVFKM0Sb9fzYo",
+  authDomain: "ema-jhon-firebase-auth-7dfc9.firebaseapp.com",
+  projectId: "ema-jhon-firebase-auth-7dfc9",
+  storageBucket: "ema-jhon-firebase-auth-7dfc9.appspot.com",
+  messagingSenderId: "380296804558",
+  appId: "1:380296804558:web:9533a884eda87f1e89389f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/__steps.js
@@ -0,0 +1,6 @@
+// 1. create a firebase project
+// 2. create web app
+// 3. npm i firebase
+// 4. save firebase config and export default app
+// 5. firebase site: build> authentication > get started >sign in method
+// 6. create sign up and login route
