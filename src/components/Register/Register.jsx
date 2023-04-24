f98b52905daf5a4882358d2c91051d4741a44ccf
swapnilRoyWeb <swapnilroyweb@gmail.com>
2023-04-25 00:05:35 +0600

    login and sign up page design ready

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -3,8 +3,23 @@ import './Register.css';
 
 const Register = () => {
     return (
-        <div>
-            <h2>Register</h2>
+        <div className='form-container'>
+            <h2 className='form-title'>Sign Up !!!</h2>
+            <form>
+                <div className='form-control'>
+                    <label htmlFor="">Email</label>
+                    <input type="email" name='email' id='' required />
+                </div>
+                <div className='form-control'>
+                    <label htmlFor="">Password</label>
+                    <input type="password" name='password' id='' required />
+                </div>
+                <div className='form-control'>
+                    <label htmlFor="">Confirm Password</label>
+                    <input type="password" name='confirm' id='' required />
+                </div>
+                <input className='btn-submit' type="submit" value="Sign Up" />
+            </form>
         </div>
     );
 };
